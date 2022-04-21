d4a97814ce6b9cacb3437b4ab05fbb81407ce2a2
Gabriel Souza <gabrielsouza_777@hotmail.com>
2022-04-21 08:19:19 -0300

    Adicionar arquivos de projeto.

---
+++ Gabriel.Software.Users.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using Gabriel.Software.Users.Console.Users;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //REMEMBER TO REMOVE
+        args = new[] {@"C:\Users\GABRI\Desktop\teste.txt"};
+        
+        if (args.Length == 0)
+        {
+            Console.WriteLine("You should pass the file path as a parameter");
+            Console.WriteLine("Example: dotnet run \"teste\" ");
+            return;
+        }
+        
+        var filePath = args[0];
+        var users = RetrieveDataPath(filePath);
+
+    }
+
+    private static IEnumerable<User> RetrieveDataPath(string filePath)
+    {
+        using var file = new StreamReader(filePath);
+        string? line;
+        while ((line = file.ReadLine()) != null)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
---
+++ Gabriel.Software.Users.Console/Users/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Gabriel.Software.Users.Console.Users;
+
+public class User
+{
+    public string UserName { get; private set; }
+
+    public User(string userName)
+    {
+        UserName = userName;
+    }
+}
